02976e06c71aa450a331ca7fbcdd06d5989d0ba9
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-08 16:45:39 -0300

    create json

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
@@ -5,14 +6,19 @@ public class Principal {
 
         Scanner lectura = new Scanner(System.in);
         ConsultaPelicula consulta = new ConsultaPelicula();
-        System.out.println("Escriba el númerode la pelicula: ");
+        System.out.println("Escriba el númerode la pelicula Start Wards: ");
 
         try {
             var numeroDePelicula = Integer.valueOf(lectura.nextLine());
             Pelicula pelicula = consulta.buscaPelicula(numeroDePelicula);
             System.out.println(pelicula);
+            GeneradorDeArchivo generador = new GeneradorDeArchivo();
+            generador.guardarJson(pelicula);
 
-        } catch (RuntimeException e) {
+        } catch (NumberFormatException e) {
+            System.out.println("Número no encontrado: " + e.getMessage());
+
+        } catch (RuntimeException | IOException e) {
             System.out.println(e.getMessage());
             System.out.println("Finalizando la aplicación");
         }
